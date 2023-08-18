06ca9813218e1011e2b044c06326aac6384c1cbf
Alinemis <127789972+Alinemis@users.noreply.github.com>
2023-08-18 20:24:07 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const startBtn = document.querySelector('[data-start]');
+const stoptBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+let timerId = null;
+
+startBtn.addEventListener('click', handlerStart);
+stoptBtn.addEventListener('click', handlerStop);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function handlerStart(evt) {
+  timerId = setInterval(() => {
+    const changeColor = getRandomHexColor();
+    body.style.backgroundColor = changeColor;
+  }, 1000);
+  startBtn.disabled = true;
+  stoptBtn.disabled = false;
+}
+
+function handlerStop(evt) {
+  clearInterval(timerId);
+  stoptBtn.disabled = true;
+  startBtn.disabled = false;
+}
